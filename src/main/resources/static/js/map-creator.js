9f6f0242818a6d9eda02d4814df95ff0dac4364c
nandor.vigh <nandor.z.vigh@gmail.com>
2019-10-25 23:44:47 +0200

    county selection

--- src/main/resources/static/js/map-creator.js
+++ src/main/resources/static/js/map-creator.js
@@ -1,5 +1,7 @@
 "use strict"
 
+let actualCountry = undefined;
+
 window.onload = function () {
     mapboxgl.accessToken = 'pk.eyJ1IjoibmVsc25vZHkiLCJhIjoiY2syNmo5cHp3MGJ4NDNyczl5ZjgxZzljZyJ9.hVyim5Txe-nXT1b7C-ucvg';
 
@@ -8,6 +10,72 @@ window.onload = function () {
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [-74.50, 40],
         zoom: 3
+    });
+
+    map.on('load', function () {
+        map.addSource('states', {
+            'type': 'geojson',
+            'data': 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson'
+        });
+        map.addLayer({
+            'id': 'states-layer',
+            'type': 'fill',
+            'source': 'states',
+            'paint': {
+                "fill-outline-color": "#666",
+                "fill-color": "rgba(0,0,0,0.1)"
+            }
+        });
+
+        map.addLayer({
+            'id': 'states-highlited',
+            'type': 'fill',
+            'source': 'states',
+            'paint': {
+                "fill-outline-color": "#00b9ff",
+                "fill-color": "#00b9ff",
+                "fill-opacity": 0.5
+            },
+            'filter': ["in", "geounit", ""]
+        });
+
+        map.addLayer({
+            'id': 'states-selected',
+            'type': 'fill',
+            'source': 'states',
+            'paint': {
+                "fill-color": "#787878",
+                "fill-opacity": 0.5
+            },
+            'filter': ["in", "geounit", ""]
+        });
+
+        map.on('click', function (e) {
+            var features = map.queryRenderedFeatures(e.point, { layers: ['states-layer'] });
+            var feature = features[0];
+
+            if (!feature || !feature.properties) {
+                return;
+            }
+
+            if(actualCountry == null){
+                actualCountry = feature.properties.geounit;
+            }
+            map.setFilter('states-highlited', ['in', 'geounit', feature.properties.geounit]);
+
+        });
+
+        map.on('mousemove', function(e){
+            var features = map.queryRenderedFeatures(e.point, { layers: ['states-layer'] });
+            var feature = features[0];
+
+            if (!feature || !feature.properties) {
+                return;
+            }
+
+            map.setFilter('states-selected', ['in', 'geounit', feature.properties.geounit]);
+        });
+
     });
 }
 
