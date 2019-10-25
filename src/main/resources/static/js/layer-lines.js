75eeb53eed0908553f35d7f92c981e2961d3d501
richardbata <richard.bata@transferwise.com>
2019-10-26 00:44:23 +0200

    bla

--- src/main/resources/static/js/layer-lines.js
+++ src/main/resources/static/js/layer-lines.js
@@ -2,6 +2,11 @@
 //
 // console.log(map);
 
+const data = [
+  [{ x: 52.099878, y: 9.994248 }, { x: 49.789746, y: 9.524684 }, { x: 49.055821, y: 19.224688 }],
+  [{ x: 51.727729, y: 20.667571 }, { x: 49.457914, y: 13.458649 }, { x: 48.501431, y: 5.243380 }],
+];
+
 window.addEventListener('load', () => {
   const geojson = {
     'type': 'FeatureCollection',
@@ -43,6 +48,8 @@ window.addEventListener('load', () => {
 
     startTime = performance.now();
 
+    var counter = 0;
+
     animateLine();
 
     document.addEventListener('visibilitychange', function () {
@@ -50,24 +57,35 @@ window.addEventListener('load', () => {
     });
 
     function animateLine(timestamp) {
-      if (resetTime) {
-        startTime = performance.now() - progress;
-        resetTime = false;
-      } else {
-        progress = timestamp - startTime;
-      }
-
-      if (progress > speedFactor * 360) {
-        startTime = timestamp;
+      console.log(counter);
+      if (counter++ === data.length) {
         geojson.features[0].geometry.coordinates = [];
-      } else {
-        var x = progress / speedFactor;
-        var y = Math.sin(x * Math.PI / 90) * 40;
-        geojson.features[0].geometry.coordinates.push([x, y]);
-        map.getSource('line-animation').setData(geojson);
+        counter = 0;
       }
+      // if (resetTime) {
+      //   startTime = performance.now() - progress;
+      //   resetTime = false;
+      // } else {
+      //   progress = timestamp - startTime;
+      // }
 
-      animation = requestAnimationFrame(animateLine);
+      // if (progress > speedFactor * 360) {
+      //   startTime = timestamp;
+      //   geojson.features[0].geometry.coordinates = [];
+      // } else {
+      // var x = progress / speedFactor;
+      // var y = Math.sin(x * Math.PI / 90) * 40;
+      // geojson.features[0].geometry.coordinates.push([x, y]);
+      for (let i = 0; i < 2; i++) {
+        const { x, y } = data[i][counter];
+        geojson.features[0].geometry.coordinates.push([y, x]);
+      }
+      map.getSource('line-animation').setData(geojson);
+      // }
+
+      setTimeout(() => {
+        animation = requestAnimationFrame(animateLine);
+      }, 1000);
     }
   });
 });
