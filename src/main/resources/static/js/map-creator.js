65e6135b690fc6fe168e4ec492956ef984cf3c69
nandor.vigh <nandor.z.vigh@gmail.com>
2019-10-25 23:50:18 +0200

    asdfasd

--- src/main/resources/static/js/map-creator.js
+++ src/main/resources/static/js/map-creator.js
@@ -1,11 +1,12 @@
 "use strict"
 
 let actualCountry = undefined;
+let map;
 
 window.onload = function () {
     mapboxgl.accessToken = 'pk.eyJ1IjoibmVsc25vZHkiLCJhIjoiY2syNmo5cHp3MGJ4NDNyczl5ZjgxZzljZyJ9.hVyim5Txe-nXT1b7C-ucvg';
 
-    var map = new mapboxgl.Map({
+    map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [-74.50, 40],
@@ -78,4 +79,5 @@ window.onload = function () {
 
     });
 }
+
 
