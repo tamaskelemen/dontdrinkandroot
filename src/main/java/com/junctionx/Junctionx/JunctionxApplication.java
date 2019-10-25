3157dcdef8b08a7cfd7f818149ff1f70af2911c2
Adam Debreceni <adam.debreceni@transferwise.com>
2019-10-25 22:17:38 +0200

    Spring hello world

---
+++ src/main/java/com/junctionx/Junctionx/JunctionxApplication.java
@@ -0,0 +1,13 @@
+package com.junctionx.Junctionx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunctionxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunctionxApplication.class, args);
+	}
+
+}
