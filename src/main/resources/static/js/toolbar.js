b6bf7d96eb1811146a84722eedfdb5271dd15455
rb10g13 <richard.bata@transferwise.com>
2019-10-26 05:49:02 +0200

    stuff

--- src/main/resources/static/js/toolbar.js
+++ src/main/resources/static/js/toolbar.js
@@ -7,9 +7,46 @@ window.addEventListener('load', () => {
       exclusive: false,
     });
 
-  $('.dropdown')
+  $('.dropdown.species')
     .dropdown({
-      clearable: true,
+      placeholder: 'Select species',
+      values: [{
+        name: 'Stock',
+        value: '3',
+        // selected: true,
+      }, {
+        name: 'Deer',
+        value: '6',
+      }, {
+        name: 'Frog',
+        value: '12',
+      }],
+    });
+
+  $('.maptype button')
+    .on('click', e => {
+      document.querySelectorAll('.maptype button')
+        .forEach(elm => elm.classList.remove('active'));
+      e.target.classList.add('active');
+    });
+
+  $('.dropdown.timespan')
+    .dropdown({
+      placeholder: 'Select timespan',
+      values: [{
+        name: 'Last 3 months',
+        value: '3',
+        selected: true,
+      }, {
+        name: 'Last 6 months',
+        value: '6',
+      }, {
+        name: 'Last 12 months',
+        value: '12',
+      }, {
+        name: 'Custom',
+        value: 'c',
+      }],
     });
 
   $('.ui.range').range({
@@ -17,6 +54,6 @@ window.addEventListener('load', () => {
     max: 30,
     start: 10,
     step: 1,
-    labelType: 'number'
+    labelType: 'number',
   });
 });
