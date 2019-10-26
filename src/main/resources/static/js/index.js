9f7e0e1867d870025bf21d4ddb19dec2f25481be
rb10g13 <richard.bata@transferwise.com>
2019-10-26 12:25:43 +0200

    ui nez stuff

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,6 @@
 import 'ion-rangeslider/js/ion.rangeSlider.min'
 import 'ion-rangeslider/css/ion.rangeSlider.min.css'
 
-import './map-creator';
+// import './map-creator';
 import './toolbar';
-import './layer-lines';
+// import './layer-lines';
