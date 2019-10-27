5e548bc036503c1c7d0b3e87423fda1d01ee0192
nandor.vigh <nandor.z.vigh@gmail.com>
2019-10-27 02:23:56 +0200

    asdfsdgds

--- src/main/resources/static/js/layer-lines.js
+++ src/main/resources/static/js/layer-lines.js
@@ -17,7 +17,7 @@ window.onload = function () {
   let territoryMaps = [];
 
   applyButton.addEventListener('click', function () {
-    const species = filterParams.species.reduce((a,b) => `${a},${b}`);
+    const species = filterParams.species === [] ? [] : filterParams.species.reduce((a,b) => `${a},${b}`);
 
     const linesIsActive = $('#lines').hasClass('active');
     const heatmapIsActive = $('#heatmap').hasClass('active');
@@ -27,19 +27,6 @@ window.onload = function () {
     const toDate = filterParams.toDate;
 
     applyButton.classList.add('loading');
-
-    if (window.test.species.includes('Stork')) {
-      fetch('http://localhost:8080//stork-data/')
-        .then(res => res.json())
-        .then(json => {
-          addLinesToMap(json, linesIsActive);
-          addHeatmapToMap(json, heatmapIsActive);
-          addTerritoryToMap(json, territoryIsActive);
-
-          applyButton.classList.remove('loading');
-        })
-        .catch(console.error);
-    }
 
     if (heatmapIsActive) {
       fetch(`http://localhost:8080/heatmap-date-range/${species}/${fromDate}/${toDate}`)
@@ -51,9 +38,13 @@ window.onload = function () {
 
           applyButton.classList.remove('loading');
         })
-        .catch(console.error);
+        .catch( error => {
+          console.error(error);
+          applyButton.classList.remove('loading');
+        });
     }
-    if (linesIsActive || territoryIsActive) {
+    if ((linesIsActive || territoryIsActive) && species !== []) {
+      console.log(fromDate);
       fetch(`http://localhost:8080/animal-path/${species}/${fromDate}/${toDate}`)
         .then(res => res.json())
         .then(json => {
@@ -63,7 +54,10 @@ window.onload = function () {
 
           applyButton.classList.remove('loading');
         })
-        .catch(console.error);
+          .catch( error => {
+            console.error(error);
+            applyButton.classList.remove('loading');
+          });
     }
   });
 
@@ -76,12 +70,8 @@ window.onload = function () {
       return animalsWays = [];
     }
     for (const value in linesData) {
-        // let lat = linesData[value].map(item =>  item.lat);
-        // let lng = linesData[value].map(item =>  item.lng);
-        // let path = bspline(lat, lng);
       let animalsWay = new google.maps.Polyline({
         geodesic: true,
-        // path: path,
         path: linesData[value],
         strokeColor: '#FF0000',
         strokeOpacity: 1.0,
@@ -125,37 +115,4 @@ window.onload = function () {
       territoryMaps.push(territoryMap);
     }
   }
-
-    function bspline(lats, lngs) {
-        let i, t, ax, ay, bx, by, cx, cy, dx, dy, lat, lng, points;
-        points = [];
-
-        for (i = 2; i < lats.length - 2; i++) {
-            for (t = 0; t < 1; t += 0.2) {
-                ax = (-lats[i - 2] + 3 * lats[i - 1] - 3 * lats[i] + lats[i + 1]) / 6;
-                ay = (-lngs[i - 2] + 3 * lngs[i - 1] - 3 * lngs[i] + lngs[i + 1]) / 6;
-
-                bx = (lats[i - 2] - 2 * lats[i - 1] + lats[i]) / 2;
-                by = (lngs[i - 2] - 2 * lngs[i - 1] + lngs[i]) / 2;
-
-                cx = (-lats[i - 2] + lats[i]) / 2;
-                cy = (-lngs[i - 2] + lngs[i]) / 2;
-
-                dx = (lats[i - 2] + 4 * lats[i - 1] + lats[i]) / 6;
-                dy = (lngs[i - 2] + 4 * lngs[i - 1] + lngs[i]) / 6;
-
-                lat = (ax * Math.pow(t + 0.1, 3)) +
-                    (bx * Math.pow(t + 0.1, 2)) +
-                    (cx * (t + 0.1)) + dx;
-
-                lng = (ay * Math.pow(t + 0.1, 3)) +
-                    (by * Math.pow(t + 0.1, 2)) +
-                    (cy * (t + 0.1)) + dy;
-
-                points.push(new google.maps.LatLng(lat, lng));
-            }
-        }
-        return points;
-    }
-
 };
