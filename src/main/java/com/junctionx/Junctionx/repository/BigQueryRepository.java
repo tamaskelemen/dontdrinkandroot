f3bbe06a788a034d8106b70b40411ff637b27e04
Keltom <tamaskelemen.kt@gmail.com>
2019-10-27 05:13:30 +0100

    Road api

--- src/main/java/com/junctionx/Junctionx/repository/BigQueryRepository.java
+++ src/main/java/com/junctionx/Junctionx/repository/BigQueryRepository.java
@@ -375,4 +375,42 @@ public class BigQueryRepository {
 
         return null;
     }
+
+    public List<List<Double>> getRoadData() {
+		try {
+			String roadQuery = "SELECT * FROM iotds.road_data;";
+			QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(roadQuery)
+					.setUseLegacySql(false).build();
+
+			JobId jobId = JobId.of(UUID.randomUUID().toString());
+			Job queryJob = bigQuery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
+
+			queryJob = queryJob.waitFor();
+
+			// Check for errors
+			if (queryJob == null) {
+				throw new RuntimeException("Job no longer exists");
+			} else if (queryJob.getStatus().getError() != null) {
+				// You can also look at queryJob.getStatus().getExecutionErrors() for all
+				// errors, not just the latest one.
+				throw new RuntimeException(queryJob.getStatus().getError().toString());
+			}
+
+			// Get the results.
+			TableResult tableResult = queryJob.getQueryResults();
+
+			List<List<Double>> result = new ArrayList<>();
+			// Print all pages of the results.
+			for (FieldValueList row : tableResult.iterateAll()) {
+				Double lat = row.get("latitude").getDoubleValue();
+				Double lng = row.get("longitude").getDoubleValue();
+				List<Double> temp = Arrays.asList(lat,lng);
+				result.add(temp);
+			}
+			return result;
+		} catch (Exception e) {
+			log.error("----------------Error:", e);
+			return null;
+		}
+	}
 }
