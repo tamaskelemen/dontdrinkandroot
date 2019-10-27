5deb5b2f589da3e9395edd4293d07e4bd2ee98d6
rb10g13 <richard.bata@transferwise.com>
2019-10-27 02:20:02 +0200

    Merge branch 'master' of https://github.com/tamaskelemen/dontdrinkandroot
    # Conflicts:
    #	src/main/resources/static/css/style.css

--- src/main/resources/static/js/layer-lines.js
+++ src/main/resources/static/js/layer-lines.js
@@ -5,19 +5,10 @@ window.onload = function () {
   const applyButton = document.getElementById('apply-button');
 
   let animalsWays = [];
-  const heatmap = new HeatmapOverlay(map,
-    {
-      'radius': 15,
-      'maxOpacity': 1,
-      latField: 'lat',
-      lngField: 'lng',
-      valueField: 'count',
-    },
-  );
   let territoryMaps = [];
 
   applyButton.addEventListener('click', function () {
-    const species = filterParams.species.reduce((a,b) => `${a},${b}`);
+    const species = filterParams.species.reduce((a, b) => `${a},${b}`);
 
     const linesIsActive = $('#lines').hasClass('active');
     const heatmapIsActive = $('#heatmap').hasClass('active');
@@ -28,18 +19,18 @@ window.onload = function () {
 
     applyButton.classList.add('loading');
 
-    if (window.test.species.includes('Stork')) {
-      fetch('http://localhost:8080//stork-data/')
-        .then(res => res.json())
-        .then(json => {
-          addLinesToMap(json, linesIsActive);
-          addHeatmapToMap(json, heatmapIsActive);
-          addTerritoryToMap(json, territoryIsActive);
-
-          applyButton.classList.remove('loading');
-        })
-        .catch(console.error);
-    }
+    // if (window.test.species.includes('Stork')) {
+    //   fetch('http://localhost:8080//stork-data/')
+    //     .then(res => res.json())
+    //     .then(json => {
+    //       addLinesToMap(json, linesIsActive);
+    //       addHeatmapToMap(json, heatmapIsActive);
+    //       addTerritoryToMap(json, territoryIsActive);
+    //
+    //       applyButton.classList.remove('loading');
+    //     })
+    //     .catch(console.error);
+    // }
 
     if (heatmapIsActive) {
       fetch(`http://localhost:8080/heatmap-date-range/${species}/${fromDate}/${toDate}`)
@@ -76,9 +67,9 @@ window.onload = function () {
       return animalsWays = [];
     }
     for (const value in linesData) {
-        // let lat = linesData[value].map(item =>  item.lat);
-        // let lng = linesData[value].map(item =>  item.lng);
-        // let path = bspline(lat, lng);
+      // let lat = linesData[value].map(item =>  item.lat);
+      // let lng = linesData[value].map(item =>  item.lng);
+      // let path = bspline(lat, lng);
       let animalsWay = new google.maps.Polyline({
         geodesic: true,
         // path: path,
@@ -102,7 +93,7 @@ window.onload = function () {
       };
     }
 
-    heatmap.setData(testData);
+    heatmapOverlay.setData(testData);
   }
 
   function addTerritoryToMap(territoryData, isActive) {
@@ -126,36 +117,36 @@ window.onload = function () {
     }
   }
 
-    function bspline(lats, lngs) {
-        let i, t, ax, ay, bx, by, cx, cy, dx, dy, lat, lng, points;
-        points = [];
+  function bspline(lats, lngs) {
+    let i, t, ax, ay, bx, by, cx, cy, dx, dy, lat, lng, points;
+    points = [];
 
-        for (i = 2; i < lats.length - 2; i++) {
-            for (t = 0; t < 1; t += 0.2) {
-                ax = (-lats[i - 2] + 3 * lats[i - 1] - 3 * lats[i] + lats[i + 1]) / 6;
-                ay = (-lngs[i - 2] + 3 * lngs[i - 1] - 3 * lngs[i] + lngs[i + 1]) / 6;
+    for (i = 2; i < lats.length - 2; i++) {
+      for (t = 0; t < 1; t += 0.2) {
+        ax = (-lats[i - 2] + 3 * lats[i - 1] - 3 * lats[i] + lats[i + 1]) / 6;
+        ay = (-lngs[i - 2] + 3 * lngs[i - 1] - 3 * lngs[i] + lngs[i + 1]) / 6;
 
-                bx = (lats[i - 2] - 2 * lats[i - 1] + lats[i]) / 2;
-                by = (lngs[i - 2] - 2 * lngs[i - 1] + lngs[i]) / 2;
+        bx = (lats[i - 2] - 2 * lats[i - 1] + lats[i]) / 2;
+        by = (lngs[i - 2] - 2 * lngs[i - 1] + lngs[i]) / 2;
 
-                cx = (-lats[i - 2] + lats[i]) / 2;
-                cy = (-lngs[i - 2] + lngs[i]) / 2;
+        cx = (-lats[i - 2] + lats[i]) / 2;
+        cy = (-lngs[i - 2] + lngs[i]) / 2;
 
-                dx = (lats[i - 2] + 4 * lats[i - 1] + lats[i]) / 6;
-                dy = (lngs[i - 2] + 4 * lngs[i - 1] + lngs[i]) / 6;
+        dx = (lats[i - 2] + 4 * lats[i - 1] + lats[i]) / 6;
+        dy = (lngs[i - 2] + 4 * lngs[i - 1] + lngs[i]) / 6;
 
-                lat = (ax * Math.pow(t + 0.1, 3)) +
-                    (bx * Math.pow(t + 0.1, 2)) +
-                    (cx * (t + 0.1)) + dx;
+        lat = (ax * Math.pow(t + 0.1, 3)) +
+          (bx * Math.pow(t + 0.1, 2)) +
+          (cx * (t + 0.1)) + dx;
 
-                lng = (ay * Math.pow(t + 0.1, 3)) +
-                    (by * Math.pow(t + 0.1, 2)) +
-                    (cy * (t + 0.1)) + dy;
+        lng = (ay * Math.pow(t + 0.1, 3)) +
+          (by * Math.pow(t + 0.1, 2)) +
+          (cy * (t + 0.1)) + dy;
 
-                points.push(new google.maps.LatLng(lat, lng));
-            }
-        }
-        return points;
+        points.push(new google.maps.LatLng(lat, lng));
+      }
     }
+    return points;
+  }
 
 };
