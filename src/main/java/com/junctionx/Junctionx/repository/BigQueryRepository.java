7d3c4e6b9e902dad42e2e8d5886e59d59fdd28bf
Keltom <tamaskelemen.kt@gmail.com>
2019-10-27 09:45:07 +0100

    pathDateRange ready to process multiple animals

--- src/main/java/com/junctionx/Junctionx/repository/BigQueryRepository.java
+++ src/main/java/com/junctionx/Junctionx/repository/BigQueryRepository.java
@@ -168,9 +168,21 @@ public class BigQueryRepository {
 		return null;
 	}
 
-	public ObjectNode pathDateRange(String animal, String from, String to) {
+	public ObjectNode pathDateRange(String animals, String from, String to) {
 		try {
-			String pathFromTo = "SELECT device_id, longitude, latitude FROM iotds.bulk_data WHERE animal like '" + animal + "' AND (longitude NOT LIKE 'NA' AND latitude NOT LIKE 'NA') AND timestamp BETWEEN  '"+ from +" 00:00:00' AND '" + to + " 23:59:59' ORDER BY timestamp ASC;";
+			String[] split = animals.split("\\+");
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < split.length; i++) {
+				sb.append("'");
+				sb.append(split[i]);
+				sb.append("'");
+				if (i != split.length - 1) {
+					sb.append(", ");
+				}
+			}
+			String joined = sb.toString();
+
+			String pathFromTo = "SELECT device_id, longitude, latitude, animal FROM iotds.bulk_data WHERE animal IN (" + joined + ") AND (longitude NOT LIKE 'NA' AND latitude NOT LIKE 'NA') AND timestamp BETWEEN  '"+ from +" 00:00:00' AND '" + to + " 23:59:59' ORDER BY timestamp ASC;";
 
 			QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(pathFromTo)
 					.setUseLegacySql(false).build();
@@ -206,7 +218,7 @@ public class BigQueryRepository {
 
 				objectNode.put("lat", lat);
 				objectNode.put("lng", lng);
-				objectNode.put("animal", animal);
+				objectNode.put("animal", row.get("animal").getStringValue());
 
 				ArrayNode temp = (ArrayNode) kulso.get(id);
 				if (temp == null) {
